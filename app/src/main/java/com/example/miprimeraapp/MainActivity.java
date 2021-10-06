44abc95e2c05ee92b4b3d111404ac7bb883a1431
ladyangel1981 <51683643+ladyangel1981@users.noreply.github.com>
2021-10-06 19:08:07 +0200

    First comment

---
+++ app/src/main/java/com/example/miprimeraapp/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.miprimeraapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.detonate);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Context context = getApplicationContext();
+                CharSequence text = "Hello toast!";
+                int duration = Toast.LENGTH_LONG;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+
+            }
+        });
+
+
+    }
+
+}
